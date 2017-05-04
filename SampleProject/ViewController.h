fa16a3952cda94185c7a91fe18e0453c294faccc
REDJEEMac <santosh@redjeemacs-mac-mini.local>
2017-05-04 15:15:17 +0530

    Initial Commit

---
+++ SampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleProject
+//
+//  Created by REDJEEMac on 04/05/17.
+//  Copyright © 2017 REDJEEMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleProject
+//
+//  Created by REDJEEMac on 04/05/17.
+//  Copyright © 2017 REDJEEMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
